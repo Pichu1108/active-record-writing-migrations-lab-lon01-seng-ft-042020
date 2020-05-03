ffda83c1bb63a8100b66a67c8acabd4946d43da5
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-03 20:59:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,10 +13,7 @@ if ENV["SCHOOL_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
 
-def drop_db
-  DB.tables.each do |table|
-    DB.execute("DROP TABLE #{table}")
-  end
+
 
   ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
