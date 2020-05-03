3e7efd961e892a2d05bfb2e45765e83226ce58b2
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-03 21:08:21 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
-class ddGradeAndBirthdateToStudents < ActiveRecord::Migration
+class ddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students, :grade, :integer,
+    add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
   end
 end
